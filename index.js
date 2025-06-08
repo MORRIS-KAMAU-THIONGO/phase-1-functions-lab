f33c772dfae51252f43af98e9d97826a41dea762
morriskamauthiongo <kamauthiongo58@gmail.com>
2025-06-08 22:40:37 +0300

    push function lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocknumber) {
+    return Math.abs(blocknumber - 42);
+}
+function distanceFromHqInFeet(blocknumber) {
+    return distanceFromHqInBlocks(blocknumber) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    }else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
