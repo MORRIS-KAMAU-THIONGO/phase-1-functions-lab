54e0403d035c89e5f32001d49f99b3a14c5d8358
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-09-06 19:55:41 +0300

    pushes functions lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264; // 1 block = 264 feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264; // Distance in blocks * feet per block
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0; // Free sample
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; // 2 cents per foot after 400 feet
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; // Flat $25 for > 2000 feet
+  } else {
+    return 'cannot travel that far'; // No rides > 2500 feet
+  }
+}
